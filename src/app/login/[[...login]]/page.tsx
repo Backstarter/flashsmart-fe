48652de8ba2eaef639c64371d4cac5b05d9e6db2
Joshua Eworo <eworojoshua@gmail.com>
2024-08-17 21:49:51 -0500

    Finished the landing page

---
+++ src/app/login/[[...login]]/page.tsx
@@ -0,0 +1,11 @@
+import { SignIn, SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <div className="flex justify-center py-24 gap-3">
+      <SignIn />
+
+      <SignUp />
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,50 @@
 import Image from "next/image";
+import { Icon } from '@iconify/react';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="relative flex min-h-screen flex-col bg-[#101010]">
+      <nav className="flex w-full p-6 justify-between">
+        <div className="font-[Fira Sans] text-xl font-bold">FLASHSMART</div>
+        <div className="flex gap-6">
+          <p className="flex items-center">
+            <span className="mr-1 text-xl flex items-center">
+              <Icon icon="material-symbols-light:add"/>
+            </span>
+            Create
+          </p>
+          <button className="bg-neutral-700 p-7 py-2 rounded-full font-semibold">Log In</button>
         </div>
-      </div>
+      </nav>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <section className="hero px-6 pt-20 w-full flex flex-col flex-1">
+        <div className="max-w-screen-xl mx-auto w-full flex">
+          <span className="text-7xl w-7/12 font-[Montserrat]">Achieve Mastery, One Card at a Time</span>
+          <div className="w-5/12 mt-8">
+            <p className="text-lg text-neutral-400 font-[Fira Sans]">Mastery is within your reach. Turn your study sessions into a journey toward success. Join now and start learning smarter.</p>
+            <button className="bg-gradient-to-r from-emerald-400 to-teal-400 p-7 py-2 rounded-full mt-5 text-lg text-black font-[Fira Sans] font-semibold">Start Now</button>
+          </div>
+          <img src="" alt="" />
+        </div>
+        <div className="cards flex-1 flex w-full">
+          <div className="flex mx-auto pt-32 gap-5">
+            <div className="w-[600px] h-[300px] bg-rose-300 rounded-xl -rotate-[10deg] p-6 text-rose-950 text-lg flex flex-col">
+              <h1 className="font-[Fira Sans] font-semibold">Efficient Learning</h1>
+              <hr className="my-3 border-t-2 border-rose-200"/>
+              <p className="pt-3">Streamline your study sessions with adaptive flashcards that focus on areas where you need the most improvement, making your learning process faster and more effective.</p>
+            </div>
+            <div className="w-[600px] h-[300px] bg-teal-300 rounded-xl rotate-[10deg] p-6 text-teal-950 text-lg flex flex-col">
+              <h1 className="font-[Fira Sans] font-semibold">Flexible Study Options</h1>
+              <hr className="my-3 border-t-2 border-teal-200"/>
+              <p className="pt-3">Study anytime, anywhere with seamless access across all your devices, allowing you to fit learning into your busy schedule with ease.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      
+      {/* Radial Glow Effect */}
+      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-full h-64 pointer-events-none">
+        <div className="absolute inset-0 bg-gradient-radial from-white via-transparent to-transparent opacity-50"></div>
       </div>
     </main>
   );
