b013d31dd86422f660e71533df68bfdb3a00110e
Joshua Eworo <eworojoshua@gmail.com>
2024-08-25 18:20:46 -0500

    unescaped apos fix

--- src/app/login/[[...login]]/page.tsx
+++ src/app/login/[[...login]]/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
     return (
         <div className="flex w-screen h-screen">
             <div className="login-bg w-4/6  flex flex-col justify-center items-center">
-                <h1 className="font-extrabold text-6xl">DON'T JUST STUDY HARD</h1> 
+                <h1 className="font-extrabold text-6xl">DON&apos;T JUST STUDY HARD</h1> 
                 <h1 className="font-extrabold text-6xl">STUDY <span className="opacity-75 logingrad drop-shadow-lg">SMART</span></h1> 
                 <p className="mt-6 font-bold">FLASHSMART</p>
             </div>
