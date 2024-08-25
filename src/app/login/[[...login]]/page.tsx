960af357ab8f3f3778ff5202c211dac417af2a93
Joshua Eworo <eworojoshua@gmail.com>
2024-08-25 18:17:26 -0500

    UX changes to login/signup

--- src/app/login/[[...login]]/page.tsx
+++ src/app/login/[[...login]]/page.tsx
@@ -4,7 +4,11 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
     return (
         <div className="flex w-screen h-screen">
-            <div className="login-bg w-4/6"></div>
+            <div className="login-bg w-4/6  flex flex-col justify-center items-center">
+                <h1 className="font-extrabold text-6xl">DON'T JUST STUDY HARD</h1> 
+                <h1 className="font-extrabold text-6xl">STUDY <span className="opacity-75 logingrad drop-shadow-lg">SMART</span></h1> 
+                <p className="mt-6 font-bold">FLASHSMART</p>
+            </div>
             <div className="w-2/6 flex flex-col items-center justify-center">
                 <SignIn />
             </div>
