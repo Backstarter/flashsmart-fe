cec24810ee92386c0399c3de1aafd62be4fdee59
Joshua Eworo <eworojoshua@gmail.com>
2024-08-25 19:25:57 -0500

    fixed nonfunctional button on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Icon } from '@iconify/react';
+import Link from "next/link";
 import { SignInButton } from "@clerk/nextjs";
 
 export default function Home() {
@@ -26,7 +27,7 @@ export default function Home() {
           <span className="text-7xl w-7/12 font-montserrat">Achieve Mastery, One Card at a Time</span>
           <div className="w-5/12 mt-8">
             <p className="text-lg text-neutral-400">Mastery is within your reach. Turn your study sessions into a journey toward success. Join now and start learning smarter.</p>
-            <button className="bg-neutral-700 p-7 py-2 rounded-full mt-5 text-lg text-white font-[Fira Sans] font-semibold">Start Now</button>
+            <Link role="button" href="/app/create" className="bg-neutral-700 p-7 py-2 rounded-full mt-5 text-lg text-white font-[Fira Sans] font-semibold">Start Now</Link>
           </div>
         </div>
         <div className="cards flex-1 flex w-full">
